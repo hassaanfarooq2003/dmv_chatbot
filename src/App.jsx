56b1e927133a811ad956242ea77a14ac3e4668af
Thou <i210442@nu.edu.pk>
2024-07-16 21:38:45 +0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Example from './Example'
+import ChatInterface from './ChatInterface'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    <ChatInterface/>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path as needed for your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+};
