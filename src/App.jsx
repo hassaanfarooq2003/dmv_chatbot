8435201dc78aa42c4c62ce79c736d7d21a701758
Thou <i210442@nu.edu.pk>
2024-07-17 15:17:31 +0500

    Added new pages
    Added Login and sIgnup. Move the chatinterface,jsx into a folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Homepage/Home'
-import ChatInterface from './ChatInterface'
+import ChatInterface from './Chatbot/ChatInterface'
+import Signup from './Auth/Signup'
+import Login from './Auth/Login'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Home/>
+<Signup/>
     
     </>
   )
